148cbbca5eac637a53ca4ca5319a5ec24ef81963
korya <korya.github@gmail.com>
2018-07-16 16:31:20 -0400

    Upgrade NodeJS to v10

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM planitar/dev-base
 USER root
 WORKDIR /
 
-# Grab Nodejs 8 and set it up
-RUN curl -sL https://deb.nodesource.com/setup_8.x | bash - && \
+# Install NodeJS 10
+RUN curl -sL https://deb.nodesource.com/setup_10.x | bash - && \
     apt-get install -y nodejs && \
     apt-get -y clean
--- test/001_node_npm_installed.sh
+++ test/001_node_npm_installed.sh
@@ -12,10 +12,10 @@ trap report ERR
 trap cleanup EXIT
 
 docker run --rm planitar/dev-nodejs \
-  node --version
+  node --version | grep -q '^v10\.'
 
 docker run --rm planitar/dev-nodejs \
-  npm --version
+  npm --version | grep -q '^6\.'
 
 docker run --rm planitar/dev-nodejs \
   node -e 'console.log(1+1)' | grep -q ^2$
